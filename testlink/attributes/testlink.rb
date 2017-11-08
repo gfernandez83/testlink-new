1fdb3b186888600412d4a990036baced94daa386
root <root@testlink.testlink.test.com>
2017-11-08 15:36:34 +0800

    first commit

---
+++ testlink/attributes/testlink.rb
@@ -0,0 +1,22 @@
+# for creation of the directories that will be mapped to the volume of the docker containers
+default['spiral']['testlinkdata']['data'] = '/testlink/data'
+default['spiral']['testlinkdata']['mariadb'] = '/testlink/mariadb'
+
+# configuration account for mariadb
+default[:spiral][:testlink][:mariadbuser] = 'root'
+default[:spiral][:testlink][:mariadbpass] = nil
+
+# configuration account for testlink
+default[:spiral][:testlink][:testlinkuser] = 'user'
+default[:spiral][:testlink][:testlinkpass] = nil 
+default[:spiral][:testlink][:testlinkmail] = 'user@example.com'
+default[:spiral][:testlink][:smtphost] = 'smtp.gmail.com'
+default[:spiral][:testlink][:smtpport] = '587'
+default[:spiral][:testlink][:smtpuser] = 'jenkins@mnltechnology.com'
+default[:spiral][:testlink][:smtppass] = nil
+default[:spiral][:testlink][:smtpconn] = 'tls'
+
+# directories needed for the backup of testlink configuration
+default['backup']['testlink']['log'] = '/opt/backup/log'
+default['backup']['testlink']['list'] = '/opt/backup/s3list' 
+default['backup']['testlink']['files'] = '/opt/backup/backup_files'
---
+++ testlink/recipes/testlink.rb
@@ -0,0 +1,100 @@
+#
+# Cookbook: QA Testlink
+#
+
+#include_recipe 'spiral::default'
+
+package 'docker.io'
+package 'docker-compose'
+package	'python-pip'
+
+service 'docker-enable-start' do
+	service_name	'docker.service'
+	action	[:enable, :start]
+end
+
+execute 'install-awscli' do
+	command 'pip install awscli'
+end
+
+directory '/Docker/' do
+	owner	'root'
+	group	'root'
+	mode	'0755'
+	action	:create
+end
+
+
+template '/Docker/docker-compose.yml' do
+	source	'testlink-docker-compose.erb'
+	owner	'root'
+	group	'root'
+	mode	'0744'
+end
+
+node['spiral']['testlinkdata'].each do |dir, path|
+	directory path do
+		action  :create
+        	owner   'root'
+        	group   'root'
+        	mode    '0755'
+	end
+end
+
+execute 'run-docker-compose' do
+	cwd	'/Docker'
+	command 'docker-compose up -d; sleep 330'
+end
+
+template '/testlink/mariadb/mariadb/conf/my.cnf' do
+        source  'testlink-mariadb.erb'
+        owner   'root'
+        group   'root'
+        mode    '0700'
+	action	:create
+end
+
+#
+# Testlink Backup
+# Note: Please add AWS secret key and access ID to server or provide IAM role to instance to have r/w access to main backup s3 bucket
+# Add script to crontab to run every 5:00 AM (UTC+8)
+# 0 21 * * * /usr/bin/nice -n19 /usr/bin/ionice -n7 -c2 /opt/backup/backup.sh
+#
+
+directory '/opt/backup' do
+        owner   'root'
+        group   'root'
+        mode    '0755'
+        action  :create
+end
+
+directory '/testlink/mariadb/mariadb/backup' do
+        owner   'root'
+        group   'root'
+        mode    '0755'
+        action  :create
+end
+
+execute 'write-upload-area' do
+	command 'chmod a+w /testlink/data/testlink/upload_area/'
+end
+
+node['backup']['testlink'].each do |dir, path|
+        directory path do
+                action  :create
+                owner   'root'
+                group   'root'
+                mode    '0755'
+        end
+end
+
+template '/opt/backup/backup.sh' do
+        source  'testlink-backup.erb'
+        owner   'root'
+        group   'root'
+        mode    '0644'
+end
+
+execute 'backup-executable' do
+	command 'chmod a+x /opt/backup/backup.sh'
+end
